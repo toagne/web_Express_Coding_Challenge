2232a9a853602a293b605e84584ddfebb340c96a
Marco Pellegrini <mpellegr@c1r5p16.hive.fi>
2024-10-14 17:16:03 +0300

    added first part of tutorial with next js - next step is to create pokemon page

---
+++ pokemon/pokemon--finder/components/pokemon-grid.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { PokemonCard } from "./pokemon-card";
+
+interface PokemonGridProps {
+    pokemonList: any
+}
+
+export function PokemonGrid({ pokemonList }: PokemonGridProps) {
+    const [ searchText, setSearchText ] = useState("");
+
+    console.log(pokemonList);
+
+    const searchFilter = (pokemonList: any) => {
+        return pokemonList.filter(
+            (pokemon: any) => pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+    }
+
+    const filteredPokemonList = searchFilter(pokemonList)
+
+    return (
+        <>
+        <div>
+            <h3 className="text-2xl py-6 text-center"> Search for your pokemon</h3>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="pokemonName">Pokemon Name</Label>
+                <Input 
+                    type="text"
+                    value={searchText}
+                    id="pokemonName"
+                    placeholder="Charizard, Pikachu, etc."
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
+            <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
+        </div>
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+            {filteredPokemonList.map((pokemon : any) => {
+                return (
+                    <PokemonCard name={pokemon.name}/>
+                )
+            })}
+        </div>
+        </>
+    )
+}
